b190566e0061001bc0ea9ed4b96a117b635f201f
R.Lohith <96762711+Lohith3699@users.noreply.github.com>
2022-06-07 09:41:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import re
+password = input(str("Input your password:"))
+x=True
+flag = 0
+while True:  
+    if (len(password)<8):
+        flag = -1
+        break
+    elif not re.search("[a-z]", password):
+        flag = -1
+        break
+    elif not re.search("[A-Z]", password):
+        flag = 2
+        break
+    elif not re.search("[0-2]", password):
+        flag = -1
+        break
+    elif not re.search("[_@$]", password):
+        flag = -1
+        break
+    elif re.search("\s", password):
+        flag = -1
+        break
+    else:
+        flag = 0
+        print("valid password")
+        break
+if flag == -1:
+  print("not a valid password")
